9fff06d0d8fa46a5b59eaca39906b73224f289db
Aiapaec64 <denizsemsit@hotmail.be>
2024-11-15 04:49:27 +0100

    hmm 2

--- discord/config.php
+++ discord/config.php
@@ -12,16 +12,13 @@ $nonce = base64_encode(random_bytes(16));
 
 if (!headers_sent()) {
     header("Content-Security-Policy: default-src 'self'; img-src 'self' https://cdn.discordapp.com data:; script-src 'self' 'unsafe-inline' https://ajax.googleapis.com https://code.jquery.com https://cdn.jsdelivr.net https://kit.fontawesome.com https://youtube.com https://youtu.be https://stackpath.bootstrapcdn.com https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://kit.fontawesome.com https://use.fontawesome.com https://stackpath.bootstrapcdn.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com https://use.fontawesome.com https://kit.fontawesome.com https://stackpath.bootstrapcdn.com; frame-src 'self' https://www.youtube.com; frame-ancestors 'none'; base-uri 'self';");
-
-    $allowed_origins = explode(',', getenv('ALLOWED_ORIGINS'));
-
+    $allowed_origins = ["https://genji.pk", "https://test.genji.pk"];
     if (in_array($_SERVER['HTTP_ORIGIN'] ?? '', $allowed_origins)) {
         header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
     }
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 }
-
 
 if (!defined('DISCORD_CLIENT_ID')) {
     define('DISCORD_CLIENT_ID', getenv('DISCORD_CLIENT_ID'));
