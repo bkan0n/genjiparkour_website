107730a71be7b732ff5b9f5c89996e30928e8983
Aiapaec64 <denizsemsit@hotmail.be>
2025-01-02 18:41:18 +0100

    blur & favicon rankcard fix

--- src/rank_card.php
+++ src/rank_card.php
@@ -14,7 +14,8 @@ include BASE_PATH . "discord/header.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Genji Parkour - Rank Card</title>
+    <title>Genji Parkour - Dashboard</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <link rel="stylesheet" href="styles/layout.css">
     <link rel="stylesheet" href="styles/rank_card.css">
     <script src="js/layout.js" defer></script>
